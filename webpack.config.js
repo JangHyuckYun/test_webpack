d267f03cf11361ce149fe0519d607d113eb514ef
JangHyuck Yun <skg09203@naver.com>
2022-04-08 17:55:36 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,109 @@
 const path = require("path");
+const glob = require("glob");
+const fs = require("fs");
 const TerserPlugin = require("terser-webpack-plugin");
 
-const userpath = path.resolve(__dirname, "src/js/user");
+const defaultServiceRoot = "../webapp/WEB-INF/static/";
+const defaultJsRoot = "src/js";
+const defaultOutputRoot = defaultServiceRoot+"js";
+const defaultOutputTestRoot = defaultServiceRoot+"testJs";
+const checkModules = [
+    ["async", "await", "promise"]
+];
 
+const getCoreJSModule = (str) => {
+  switch(str) {
+      case "async": case "await": case "promise":
+          return "";
 
-module.exports = {
-    mode:"development",
-    entry: [
-        "core-js/stable/promise",
-        userpath + "/testUser.js"
-    ],
-    output: {
-        path: path.resolve(__dirname, "dist/js"),
-        filename: "[name]_bundle.js"
-    },
-    target: ['web', 'es5'],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [[
-                            "@babel/preset-env",{
-                                "useBuiltIns" : "entry",
-                                "corejs": 3,
-                                "targets" : {
-                                    "browsers": ["ie >= 10"]
-                                },
-                                "shippedProposals": true
-                            }
-                        ]],
-                        plugins: [
-                            "@babel/plugin-transform-runtime"
-                        ]
+      default:
+          return "";
+  }
+};
+
+const getEntry = (dirRange) => {
+    let entry = {};
+
+    const addEntry = (url) => {
+        let _import = [];
+        let fileContent = fs.readFileSync(url).toString().toLowerCase();
+
+        checkModules.forEach(checkModule => {
+            checkModule.every(module => {
+                console.log(url, fileContent.includes(module));
+                if(fileContent.includes(module)) {
+                    let getModule = getCoreJSModule(module);
+
+                    if(getModule) {
+                        _import.push(getCoreJSModule(module))
+                    }
+                    return false;
+                }
+
+                return true;
+            })
+
+        });
+
+        _import.push(url);
+
+        let filePath = url.split(defaultJsRoot)[1];
+        let fileName = filePath.split("/").slice(-1)[0].split(".")[0];
+
+        console.log("_import", _import);
+
+        entry = {
+            ...entry,
+            [fileName]: {
+                import: _import,
+                filename: filePath.substr(1) // substr()로 맨 앞 슬래시( "/" ) 제거
+            }
+        }
+    };
+
+    glob.sync(dirRange).forEach(file => addEntry(file));
+
+    return entry;
+}
+
+const doAsync = async (env) => {
+    const entry = getEntry(path.resolve(__dirname,`${defaultJsRoot}/**/*.js`));
+    console.log("entry", entry);
+    return {
+        mode:env.dev ? "development" : "production",
+        entry,
+        output: {
+            path: path.resolve(__dirname, defaultOutputTestRoot),
+            filename: "[name].js"
+        },
+        target: ['web', 'es5'],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            sourceType:"unambiguous",
+                            presets: [[
+                                "@babel/preset-env",{
+                                    "useBuiltIns" : "usage",
+                                    "corejs": 3,
+                                    "targets" : {'ie':"9" },
+                                    "shippedProposals": true
+                                }
+                            ]]
+                        }
                     }
                 }
-            }
-        ]
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin()]
-    },
-    devtool: 'source-map'
-}
+            ]
+        },
+        optimization: {
+            minimize: true,
+            minimizer: [new TerserPlugin()]
+        }
+    };
+};
+
+module.exports = doAsync;
